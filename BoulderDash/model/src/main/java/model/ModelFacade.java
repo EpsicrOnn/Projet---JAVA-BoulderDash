add3b708c14464075c7e6d9691f509461e1fb1ff
VincentVexia <vallet.vincent13@gmail.com>
2017-06-20 17:06:30 +0200

    Facade amélioration by Vincent

--- BoulderDash/model/src/main/java/model/ModelFacade.java
+++ BoulderDash/model/src/main/java/model/ModelFacade.java
@@ -25,7 +25,8 @@ public class ModelFacade implements IModel {
      * @see model.IModel#getExampleById(int)
      */
     @Override
-    public Example getExampleById(final int id) throws SQLException {
+    public Example getExampleById(final int id) 
+    		throws SQLException {
         return ExampleDAO.getExampleById(id);
     }
 
@@ -34,7 +35,8 @@ public class ModelFacade implements IModel {
      * @see model.IModel#getExampleByName(java.lang.String)
      */
     @Override
-    public Example getExampleByName(final String name) throws SQLException {
+    public Example getExampleByName(final String name) 
+    		throws SQLException {
         return ExampleDAO.getExampleByName(name);
     }
 
@@ -43,7 +45,8 @@ public class ModelFacade implements IModel {
      * @see model.IModel#getAllExamples()
      */
     @Override
-    public List<Example> getAllExamples() throws SQLException {
+    public List<Example> getAllExamples() 
+    		throws SQLException {
         return ExampleDAO.getAllExamples();
     }
 
