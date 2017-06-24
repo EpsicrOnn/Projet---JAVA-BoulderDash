b7b37ffb9b6ec0424b62c418c057c6f2d73f9537
VincentVexia <vallet.vincent13@gmail.com>
2017-06-24 16:12:33 +0200

    test

--- BoulderDash/model/src/main/java/model/Sprite.java
+++ BoulderDash/model/src/main/java/model/Sprite.java
@@ -1,4 +1,4 @@
-package model.element;
+package model;
 
 import java.awt.Image;
 import java.io.File;
@@ -7,57 +7,54 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Sprite {
+
 	private Image image;
 	private String imageName;
+	private int consoleImage;
 	private boolean imageLoaded;
-	private int databaseIDImage;
 
-	public Sprite(final int databaseIDImage, final String imageName) {
-		this.setDatabaseIDImage(databaseIDImage);
+	public Sprite(final int i, final String imageName) {
+		this.setConsoleImage(i);
 		this.setImageName(imageName);
-		// met en place le nom de l'image
 	}
 
-	public int getDatabaseIDImage() {
-		return this.databaseIDImage;
+	public Sprite(final char character) {
+		this(character, "noimage.jpg");
 	}
 
-	public Image getImage() {
+	public final Image getImage() {
 		return this.image;
 	}
 
-	public boolean getImageLoaded() {
-		return this.imageLoaded;
+	public final void loadImage() throws IOException {
+		this.setImage(ImageIO.read(new File("images/" + this.getImageName())));
 	}
 
-	public String getImageName() {
-		return this.imageName;
+	public final int getConsoleImage() {
+		return this.consoleImage;
 	}
 
-	public void loadImage() {
-		try {
-			this.setImage(ImageIO.read(new File("Sprites/" + this.getImageName())));
-		} catch (final IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// met l'image lue en paramètre grâce à la récupération du nom de
-		// celle-ci
-	}
-
-	public void setDatabaseIDImage(final int databaseIDImage) {
-		this.databaseIDImage = databaseIDImage;
-	}
-
-	public void setImage(final Image image) {
+	private void setImage(final Image image) {
 		this.image = image;
 	}
 
-	public void setImageLoaded(final boolean imageLoaded) {
-		this.imageLoaded = imageLoaded;
+	private void setConsoleImage(final int i) {
+		this.consoleImage = i;
 	}
 
-	public void setImageName(final String imageName) {
+	public final String getImageName() {
+		return this.imageName;
+	}
+
+	private void setImageName(final String imageName) {
 		this.imageName = imageName;
+	}
+
+	public final boolean isImageLoaded() {
+		return this.imageLoaded;
+	}
+
+	public final void setImageLoaded(final boolean isImageLoaded) {
+		this.imageLoaded = isImageLoaded;
 	}
 }
