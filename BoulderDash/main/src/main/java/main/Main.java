d7b75587fa111145c98165fa88be4262fce04476
VincentVexia <vallet.vincent13@gmail.com>
2017-06-24 16:34:28 +0200

    javadoc main

--- BoulderDash/main/src/main/java/main/Main.java
+++ BoulderDash/main/src/main/java/main/Main.java
@@ -9,6 +9,7 @@ import model.IBoulderDashModel;
 import view.BoulderDashView;
 import view.IBoulderDashView;
 
+// TODO: Auto-generated Javadoc
 /**
  * <h1>The Class Main.</h1>
  *
@@ -16,15 +17,17 @@ import view.IBoulderDashView;
  * @version 1.0
  */
 public abstract class Main {
+	
+	/** The levelid. */
 	private static int LEVELID = 1;
 
 	/**
 	 * The main method.
 	 *
-	 * @param args
-	 *            the arguments
-	 * @throws IOException
-	 * @throws InterruptedException
+	 * @param args            the arguments
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws InterruptedException the interrupted exception
+	 * @throws SQLException the SQL exception
 	 */
 	public static void main(final String[] args) throws IOException, InterruptedException, SQLException {
 		final IBoulderDashModel model = new BoulderDashModel(Main.LEVELID);
